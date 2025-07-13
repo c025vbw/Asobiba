a95d20db8aa6e840ea4af65d56100face9a76035
a2c6201 <a2c6201@gmail.com>
2025-07-13 22:16:50 +0900

    chore: eslintとPrettierのsetup

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,28 +1,42 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import react from "eslint-plugin-react";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist", "node_modules"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintConfigPrettier,
+    ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      "no-unused-vars": "warn", // 未使用変数の警告
+      "no-console": "warn", // console.log などの警告
+      "eqeqeq": ["error", "always"], // 厳密等価演算子の強制
+      "react/jsx-boolean-value": ["warn", "never"], // boolean propsは値省略
+      "react/jsx-no-undef": "error", // 未定義コンポーネントの禁止
+      "react/jsx-uses-react": "off", // React 17+では不要
+      "react/jsx-uses-vars": "warn", // JSXで使われる変数の警告
     },
   },
-)
+);
