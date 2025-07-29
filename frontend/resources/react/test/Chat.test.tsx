deb3e1f87febf89600687c69bdba520a993d1083
a2c6201 <a2c6201@gmail.com>
2025-07-29 23:13:23 +0900

    chore: jest-domの導入

--- frontend/resources/react/test/Chat.test.tsx
+++ frontend/resources/react/test/Chat.test.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import Chat from "../components/Chat";
 
 test("ユーザーがコメントを入力して送信できる", () => {
   render(<Chat />);
-  const input = screen.getByPlaceholderText("コメントを入力");
+  const input = screen.getByPlaceholderText("質問してみましょう");
   const button = screen.getByText("送信");
 
   fireEvent.change(input, { target: { value: "こんにちは" } });
